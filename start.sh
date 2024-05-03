022bdce9f461e96834d2be9f365d18536005f9f3
andromedagalax <andromedagalaxynearas@gmail.com>
2024-05-03 12:41:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,3 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/andromedagalax/Filter_Bot /Nobita-Filter-Bot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Nobita-Filter-Bot
-fi
-cd /Nobita-Filter-Bot
 pip3 install -U -r requirements.txt
 echo "Starting...."
 python3 bot.py
